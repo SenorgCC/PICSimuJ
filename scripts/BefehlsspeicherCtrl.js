b50cee65186d5053149a4127242569872df03383
SenorgCC <Alex.Sinicyn@gmx.de>
2016-04-29 09:04:51 +0200

    - Maincontroller zerlegt in mehrere Dateien

--- scripts/BefehlsspeicherCtrl.js
+++ scripts/BefehlsspeicherCtrl.js
@@ -34,3 +34,25 @@ app.controller('Befehlsspeichercontroller', function ($scope) {
 
 
 });
+//Angularmagic zum Parsen von Dateien
+//Schwarze Magie
+app.directive('onReadFile', function ($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function (scope, element, attrs) {
+            var fn = $parse(attrs.onReadFile);
+
+            element.on('change', function (onChangeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (onLoadEvent) {
+                    scope.$apply(function () {
+                        fn(scope, {$fileContent: onLoadEvent.target.result});
+                    });
+                };
+
+                reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+            });
+        }
+    };
+});
