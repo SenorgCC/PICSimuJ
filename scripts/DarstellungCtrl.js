d0bf31787a0551fffcf27f30273e10b9a3ffff56
harm86 <harmischweizer@googlemail.com>
2016-05-02 16:23:00 +0200

    Taktauswahl erweitert, darstellungctrl.js wurde bereinigt

--- scripts/DarstellungCtrl.js
+++ scripts/DarstellungCtrl.js
@@ -2,32 +2,23 @@
  * Created by harmi on 29.04.2016.
  */
 app.controller('DropdownCtrl', function ($scope) {
-
-
+    
     $(document).ready(function () {
         $('.selectpicker').selectpicker({
             style: 'btn-info'
         });
         $('.selectpicker').on('change',function () {
             $scope.Takt = $(this).val();
-            alert($scope.Takt);
-
         });
-
-
     });
 });
 
 app.controller('UploadCtrl', function ($scope) {
     $(document).ready(function () {
-
         $('#uploadBtn').change(function() {
             var filename = $('#uploadBtn').val().split('\\').pop();
             $('#uploadFile').val(filename);
         });
-
     });
-
-
 });
 
