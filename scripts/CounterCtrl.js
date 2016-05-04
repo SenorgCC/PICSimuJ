61d981f697dcbc739fd249378394aa10397b9c35
harm86 <harmischweizer@googlemail.com>
2016-05-04 16:06:15 +0200

    Taktauswahl angepasst.

--- scripts/CounterCtrl.js
+++ scripts/CounterCtrl.js
@@ -2,7 +2,7 @@ app.controller('Counter', function ($scope){
     $scope.Takt=1;
     $scope.Laufzeit=0;
    $scope.Zeit = function (takte){
-        $scope.Laufzeit = $scope.Laufzeit + (1 / $scope.Takt * takte);
+        $scope.Laufzeit = $scope.Laufzeit + ((1 / $scope.Takt) * takte);
         alert ($scope.Laufzeit);
     };
     //$scope.Counter=Zeit(takte);
