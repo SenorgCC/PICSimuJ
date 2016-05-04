be61533f9fb413d700148244813adc04e9bde204
harm86 <harmischweizer@googlemail.com>
2016-05-04 18:12:51 +0200

    Neues Dropdownmenü mit den fehlenden Werten befüllt.

--- scripts/DarstellungCtrl.js
+++ scripts/DarstellungCtrl.js
@@ -13,9 +13,15 @@ app.controller('DropdownCtrl',['$scope', function ($scope, storage) {
     });*/
     $scope.items=
         [
-        {id: '1', name : '2 MHz', Takt: 2},
-        {id: '2', name : '4 MHz', Takt: 4},
-        {id: '3', name : '8 MHz', Takt: 8}
+            {id: '1', name : '32,767 kHz', Takt : 0.032767},
+            {id: '2', name : '500 kHz', Takt : 0.5},
+            {id: '3', name : '1 MHz', Takt: 1},
+            {id: '4', name : '2 MHz', Takt: 2},
+            {id: '5', name : '4 MHz', Takt: 4},
+            {id: '6', name : '8 MHz', Takt: 8},
+            {id: '7', name : '16 MHz', Takt: 16},
+            {id: '8', name : '20 MHz', Takt : 20},
+            {id : '9', name : '32 MHz', Takt : 32}
         ];
     $scope.selectedItem = $scope.items[0];
 
