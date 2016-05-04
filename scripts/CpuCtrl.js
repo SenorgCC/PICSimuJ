1ccaacfd62f649c1c32dbe57c3679c165c19772d
SenorgCC <Alex.Sinicyn@gmx.de>
2016-05-04 23:08:32 +0200

    Return befehle beendet, Kommentierung angefangen

--- scripts/CpuCtrl.js
+++ scripts/CpuCtrl.js
@@ -2,28 +2,40 @@ app.controller('CPU', function ($scope) {
     $scope.Instructioncounter=0;
 
     function getDirectory(binOP) {
+        //Das D-Bit befindet sich bei den meisten Befehlen an der Selben Position, daher kann es ausmaskiert werden
         var directory = parseInt(binOP, 2) & parseInt('00000010000000', 2);
+        //Die ">>" Bewirken einen Rotate right um eine Anzahl von Stellen
+        //Da das Directory Bit sich an der 8. Stelle befindet muss es um 7 Stellen rotiert werden
+        //Dies vereinfacht die weitere Verarbeitung, da es nur noch auf "1" überprüft werden muss
         directory = directory >> 7;
         return directory;
     }
 
     function getFileregister(binOP) {
+        //Die File-Bits werden bei diesem Befehl ausmaskiert
+        //JS macht bei einer Verundung von von Strings probleme
+        //daher müssen diese zuerst in einen Integer gecastet werden
         var file = parseInt(binOP, 2) & parseInt('00000001111111', 2);
         return file;
     }
 
     function getbitAddress(binOP) {
+        //Selbiges Prinzip wie bei getFileregister und getDirectory
+        //zum einfacheren Verarbeiten wird das ergebnis um 7 stellen nach rechts rotiert
         var bitAddress = parseInt(binOP, 2) & parseInt('00001110000000', 2);
         bitAddress = bitAddress >> 7;
         return bitAddress;
     }
 
     function getLiteralfieldshort(binOP) {
+        //Einfache Maskierung, wie bei den anderen Befehlen
+        //Desweiteren gibt es hier 2 Literalmaskierungsfunktionen, da es befehle gibt zu 11 statt 7 Bit liefern
         var literalfield = parseInt(binOP, 2) & parseInt('00000011111111', 2);
         return literalfield;
     }
 
     function getLiteralfieldlong(binOP) {
+        //Die Befehle GOTO und CALL benötigen ein 11 Bit Literal, statt einem 7 Bit großen
         var literalfield = parseInt(binOP, 2) & parseInt('00011111111111', 2);
         return literalfield;
     }
@@ -43,8 +55,8 @@ app.controller('CPU', function ($scope) {
     }
     function getBinaryLiteralArray(IntVal) {
 
-        //Das k Literalfeld wird mit 11 Bit Kodiert der Programmcounter Benötigt 13, diese 11 werden in einem BinArray
-        //gespeichert
+        //Das k Literalfeld wird mit 11 Bit Kodiert der Programmcounter Benötigt 13
+        //diese 11 werden in einem BinArray gespeichert
         var result_Binary = [];
 
         for (var i = 0; i < 11; i++) {
@@ -54,19 +66,24 @@ app.controller('CPU', function ($scope) {
 
     }
 
-    function convertArrayToHex(binValue) {
+    function convertArrayToHex(BinArray) {
+        //Um eine Korrekte Umwandlung von einem Binär Array zu einem Hex wert zu ermöglichen
+        //wird das übergebene Bit Array Bitweise zusammengesetzt und im Anschluss konvertiert
+        //Diese Funktion umgeht das MSB und LSB problem zwischen JS und der Ausgabe
         var result = "";
-        result = binValue[7].toString() + binValue[6].toString()
-            + binValue[5].toString() + binValue[4].toString()
-            + binValue[3].toString() + binValue[2].toString()
-            + binValue[1].toString() + binValue[0].toString();
+        result = BinArray[7].toString() + BinArray[6].toString()
+            + BinArray[5].toString() + BinArray[4].toString()
+            + BinArray[3].toString() + BinArray[2].toString()
+            + BinArray[1].toString() + BinArray[0].toString();
         result = parseInt(result, 2);
         result = result.toString(16);
         return result;
     }
 
     function getComArray(binArray){
+        //Diese Funktion Wandelt das eingegebene binär Array in das ensprechende komplimentäre array um und gibt es aus
         var complement=new Array();
+        //Da nur 8 Bit Werte übergeben werden reicht eine abfrage bis 8
         for (var i=0; i<8;i++){
             if(binArray[i]==0){
                 complement[i]=1;
@@ -79,6 +96,8 @@ app.controller('CPU', function ($scope) {
     }
 
     function getZweierKomplement(hexValue){
+        //Das Zweierkomplement wird durch die Negation der Binär zahl und anschließender Addition von 1 errechnet
+        //Da die Werte als Hex gespeichert werden müssen diese umgewandelt werden
         var zweierKompResult= getBinaryArray(hexValue)
         zweierKompResult= getComArray(zweierKompResult);
         zweierKompResult = convertArrayToHex(zweierKompResult);
@@ -621,17 +640,17 @@ app.controller('CPU', function ($scope) {
 
             //Das Literalarray enthält 11 Bit und PCLATH 2 Bit damit der PC die nötigen 13 Bit größe bekommt,
             //Müssen diese mit dem Concat Befehl konkateniert werden
-            $scope.ProgramCounter= PCLATH43.concat(literalArray);
+            var PCLnewArray= PCLATH43.concat(literalArray);
 
             //Der Join Befehl enfernt die "," aus einem Array. Dadurch entsteht eine Binäre Zahl, die zu einem Int
-            //Umgewandelt werden kann
-            var PCLBefehl=parseInt($scope.ProgramCounter.join(''),2);
+            //Umgewandelt werden kann und dannach zu einem Hex String
+            var PCLBefehl=parseInt(PCLnewArray.join(''),2);
 
             //Umwandlung der Integer Zahl in einen Hex String wert
             PCLBefehl = PCLBefehl.toString(16);
 
-            //Der Hex String wird zur übergabe des neuen Befehels benötigt
-            callOperation(PCLBefehl);
+            $scope.ProgramCounter=PCLBefehl;
+
         },
         "CRLWDT": function () {
             //DO SOMETHING
@@ -640,7 +659,31 @@ app.controller('CPU', function ($scope) {
             $scope.PowerDownbit=1;
         },
         "GOTO": function (k) {
-            //DO SOMETHING
+            ///TODO: Irgendwie Testen...
+
+            //Das PCLATH wird als Hex Zahl gelagert, zum verarbeiten wird es aber als Bit array Benötigt, daher Umwandlung
+            var PCLATHarray=getBinaryArray($scope.PCLATH);
+
+            //Zur Vereinfachten Weiterverarbeitung wird ein neues Array mit einer 2 Bit größe erstellt
+            //In dieses werden das 4. und 3. Bit des PCLATH gespeichert
+            var PCLATH43=new Array(2);
+
+            //Laden der 2 PCLATH bits in das Verarbeitungsarray
+            PCLATH43[0]=PCLATHarray[3];
+            PCLATH43[1]=PCLATHarray[4];
+
+            //Das Literalarray enthält 11 Bit und PCLATH 2 Bit damit der PC die nötigen 13 Bit größe bekommt,
+            //Müssen diese mit dem Concat Befehl konkateniert werden
+            var PCLnewArray= PCLATH43.concat(literalArray);
+
+            //Der Join Befehl enfernt die "," aus einem Array. Dadurch entsteht eine Binäre Zahl, die zu einem Int
+            //Umgewandelt werden kann
+            var PCLBefehl=parseInt(PCLnewArray.join(''),2);
+
+            //Umwandlung der Integer Zahl in einen Hex String wert
+            PCLBefehl = PCLBefehl.toString(16);
+
+            $scope.ProgramCounter=PCLBefehl;
 
         },
         "IORLW": function (k) {
@@ -663,13 +706,31 @@ app.controller('CPU', function ($scope) {
             $scope.ProgramStack.pop();
         },
         "RETLW": function (k) {
-            //DO SOMETHING
+            ///TODO: TESTEN!
+            //Der übergebene Literal muss vor der Speicherung in einen Hexwert umgewandelt werden
+            $scope.w_reg= k.toString(16);
+            //Das Top of Stack von ProgramStack wird durch die maxlänge-1 bestimmt, da es keine native fkt dafür gibt
+            //Und im ProgramCounter abgespeichert
+            $scope.ProgramCounter=$scope.ProgramStack[$scope.ProgramStack.length-1];
+            //Nach dem Übertrag wird der TOS vom ProgramStack gelöscht
+            $scope.ProgramStack.pop();
         },
         "RETURN": function () {
-            //DO SOMETHING
+            //Wie RETLW nur ohne Literalübergabe
+            //Das Top of Stack von ProgramStack wird durch die maxlänge-1 bestimmt, da es keine native fkt dafür gibt
+            //Und im ProgramCounter abgespeichert
+            $scope.ProgramCounter=$scope.ProgramStack[$scope.ProgramStack.length-1];
+            //Nach dem Übertrag wird der TOS vom ProgramStack gelöscht
+            $scope.ProgramStack.pop();
         },
         "SLEEP": function () {
-            //DO SOMETHING
+            ///TODO: Wie zur Hölle soll man des Simulieren ??????????
+            $scope.watchdogtimer='00';
+            $scope.wdtPrescaler=0;
+            $scope.TimeOutbit=1;
+            $scope.PowerDownbit=0;
+            // Hier sollte sowas wie ein Sleep kommen, aber kp wie der umzusetzten ist ...
+            // Absolut kp
         },
         "SUBLW": function (k) {
             var zahl1=k;
