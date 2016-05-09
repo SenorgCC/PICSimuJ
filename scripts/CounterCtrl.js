0a914ce08671edb7a58217d99457f9f5994374f8
harm86 <harmischweizer@googlemail.com>
2016-05-09 16:00:27 +0200

    CounterCtrl  und Testverzeichnis entfernt

--- scripts/CounterCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller('Counter', function ($scope, DataPic){
-    $scope.Zeit = function (takte) {
-        DataPic.Laufzeit= DataPic.Laufzeit + (1/DataPic.Takt)* takte;
-        alert(DataPic.Laufzeit);
-        ///TODO: Alert entfernen
-    }
-
-
-});
