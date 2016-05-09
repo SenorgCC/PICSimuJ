b47a26dd706ec40353e8b842f5ea69437301c37f
harm86 <harmischweizer@googlemail.com>
2016-05-09 13:44:48 +0200

    CounterCtrl bereinigt

--- scripts/CounterCtrl.js
+++ scripts/CounterCtrl.js
@@ -1,12 +1,5 @@
 app.controller('Counter', function ($scope){
     $scope.Laufzeit=0;
 
-    //$scope.items = storage.get();
-    /*
-   $scope.Zeit = function (takte){
-        $scope.Laufzeit = $scope.Laufzeit + ((1 / $scope.selectedItem.Takt) * takte);
-        alert ($scope.Laufzeit);
-       alert($scope.items);
-    };
-    */
+    
 });
