7f063a6a1baa55b00060d41c2208202e3a40a48d
harm86 <harmischweizer@googlemail.com>
2016-05-09 15:16:17 +0200

    Taktberechnung implementiert

--- scripts/CounterCtrl.js
+++ scripts/CounterCtrl.js
@@ -1,8 +1,8 @@
 app.controller('Counter', function ($scope, DataPic){
     $scope.Zeit = function (takte) {
-        alert(DataPic.Takt);
         DataPic.Laufzeit= DataPic.Laufzeit + (1/DataPic.Takt)* takte;
         alert(DataPic.Laufzeit);
+        ///TODO: Alert entfernen
     }
 
 
